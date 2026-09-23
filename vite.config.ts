e653fbb9825265af739478eddaef84d6e62d98d9
sairahul5457 <sairahul5457m@gmail.com>
2026-09-23 13:37:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig(({ mode }) => ({
   base: "/health-minder-pro/",
 
